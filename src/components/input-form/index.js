e2abd75c59727d2326b44b5cb3ac0014234b4578
Cuse89 <marcusguthrie111@gmail.com>
2020-05-05 17:14:01 +0200

    create InputForm

---
+++ src/components/input-form/index.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./InputForm.module.scss";
+
+const InputForm = ({
+  inputType,
+  placeholder,
+  autoComplete,
+  onChange,
+  onSubmit,
+  submitText,
+  validateInput,
+  label
+}) => {
+  const [inputValue, setInputValue] = useState("");
+  const isValid = validateInput(inputValue);
+  const handleOnChange = e => {
+    e.preventDefault();
+    onChange(e.target.value);
+    setInputValue(e.target.value);
+  };
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    onSubmit(inputValue);
+    setInputValue("");
+  };
+
+  return (
+    <form className={styles.root} onSubmit={handleOnSubmit}>
+      {label && <label>{label}</label>}
+      <input
+        type={inputType}
+        onChange={handleOnChange}
+        placeholder={placeholder}
+        value={inputValue}
+        autoComplete={autoComplete}
+      />
+      {onSubmit && isValid && (
+        <input
+          className={styles.submit}
+          type="submit"
+          value={submitText}
+          disabled={!isValid}
+        />
+      )}
+    </form>
+  );
+};
+
+InputForm.defaultProps = {
+  inputType: "text",
+  placeholder: "",
+  autoComplete: "off",
+  onChange: () => {},
+  submitText: "Submit",
+  validateInput: () => true,
+  label: ""
+};
+
+InputForm.propTypes = {
+  inputType: PropTypes.string,
+  placeholder: PropTypes.string,
+  autoComplete: PropTypes.oneOf(["on", "off"]),
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+  submitText: PropTypes.string,
+  validateInput: PropTypes.func,
+  label: PropTypes.string
+};
+
+export default InputForm;
