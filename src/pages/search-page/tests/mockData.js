c7c64c4bbb3579fe8d5b65c3cc3d87b40291a75e
Cuse89 <marcusguthrie111@gmail.com>
2020-05-11 14:54:19 +0200

    SearchPage tests

--- src/pages/search-page/tests/mockData.js
+++ src/pages/search-page/tests/mockData.js
@@ -1,15 +1,44 @@
-export const moviesResponse = {
-    results: [
-        {
-            id: 1,
-            title: "Mock movie 1"
-        },
-        {
-            id: 2,
-            title: "Mock movie 2"
-        }
-    ],
-    page: 1,
-    total_results: 2,
-    total_pages: 1
+export const movies = {
+  results: [
+    {
+      id: 1,
+      title: "Mock movie 1"
+    },
+    {
+      id: 2,
+      title: "Mock movie 2"
+    }
+  ],
+  page: 1,
+  total_results: 4,
+  total_pages: 2
+};
+
+export const moviesPage2 = {
+  results: [
+    {
+      id: 3,
+      title: "Mock movie 3"
+    },
+    {
+      id: 4,
+      title: "Mock movie 4"
+    }
+  ],
+  page: 2,
+  total_results: 4,
+  total_pages: 2
+};
+
+export const moviesExtraInfo = {
+  results: [
+    {
+      id: 1,
+      title: "Mock movie 1",
+      extraInfo: "info"
+    }
+  ],
+  page: 1,
+  total_results: 2,
+  total_pages: 1
 };
