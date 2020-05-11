6f82e94d4fc9acfece79bf21216c7ef47832f5e0
Cuse89 <marcusguthrie111@gmail.com>
2020-05-11 10:23:48 +0200

    Not found page implementation

--- src/pages/app/index.js
+++ src/pages/app/index.js
@@ -3,16 +3,18 @@ import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import SearchPage from "pages/search-page";
 import MoviePage from "pages/movie-page";
 import Header from "components/header";
+import NotFoundPage from "pages/not-found-page";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-          <Header/>
+        <Header />
         <Switch>
-          <Route exact path="/" render={() => <Redirect to="/search" />}   />
-<Route path="/search" component={SearchPage}/>
-          <Route path="/movie/:id" component={MoviePage}/>
+          <Route exact path="/" render={() => <Redirect to="/search" />} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/movie/:id" component={MoviePage} />
+          <Route component={NotFoundPage} />
         </Switch>
       </BrowserRouter>
     </div>
