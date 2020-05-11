b5284d524e8fb7640710f748c63c35a6e6ce6c87
Cuse89 <marcusguthrie111@gmail.com>
2020-05-11 15:46:00 +0200

    Pagination propTypes

--- src/components/pagination/index.js
+++ src/components/pagination/index.js
@@ -1,8 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome/index";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-import styles from "./Pagination.module.scss"
+import styles from "./Pagination.module.scss";
 
 const Pagination = ({
   page,
@@ -13,14 +14,29 @@ const Pagination = ({
   return (
     <div className={styles.root}>
       {page > 1 && (
-        <FontAwesomeIcon className={styles.leftArrow} icon={faArrowLeft} onClick={onPreviousPageClick} />
+        <FontAwesomeIcon
+          className={styles.leftArrow}
+          icon={faArrowLeft}
+          onClick={onPreviousPageClick}
+        />
       )}
       Page {page} out of {totalPages}
       {page < totalPages && (
-        <FontAwesomeIcon className={styles.rightArrow} icon={faArrowRight} onClick={onNextPageClick} />
+        <FontAwesomeIcon
+          className={styles.rightArrow}
+          icon={faArrowRight}
+          onClick={onNextPageClick}
+        />
       )}
     </div>
   );
+};
+
+Pagination.propTypes = {
+  page: PropTypes.number.isRequired,
+  onPreviousPageClick: PropTypes.func.isRequired,
+  onNextPageClick: PropTypes.func.isRequired,
+  totalPages: PropTypes.number.isRequired
 };
 
 export default Pagination;
