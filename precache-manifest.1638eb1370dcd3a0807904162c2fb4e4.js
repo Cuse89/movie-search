1e6966af0b6b7ad0da5ad9b3c96ca7e7ca0a126c
Cuse89 <marcusguthrie111@gmail.com>
2020-05-11 16:25:27 +0200

    Updates

---
+++ precache-manifest.1638eb1370dcd3a0807904162c2fb4e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a26f16e49a2e294bc2da1c52f8581272",
+    "url": "/movie-search/index.html"
+  },
+  {
+    "revision": "877528530d05617ee2a7",
+    "url": "/movie-search/static/css/main.0e52fd73.chunk.css"
+  },
+  {
+    "revision": "0c41c0cb9fdc84b11df0",
+    "url": "/movie-search/static/js/2.98a0a835.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/movie-search/static/js/2.98a0a835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877528530d05617ee2a7",
+    "url": "/movie-search/static/js/main.f4c03ef9.chunk.js"
+  },
+  {
+    "revision": "08d1cf6968dfcbb7f688",
+    "url": "/movie-search/static/js/runtime-main.fd4c7407.js"
+  }
+]);
